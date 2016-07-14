1070d4685fe8c9f417fb5c16d1af188d317053ba
jlmcgehee21 <jlmcgehee21@gmail.com>
2016-07-14 18:43:29 -0400

    changed sqlacodegen location

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "[OPTIONAL] your-postgres-url" != "{{ cookiecutter.postgres_url }}" ]; then
-    sqlacodegen '{{ cookiecutter.postgres_url }}' >> ./src/data/local/models.py
+    sqlacodegen '{{ cookiecutter.postgres_url }}' >> ./src/data/external/models.py
 fi
